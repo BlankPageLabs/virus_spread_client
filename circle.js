65cee1e7cdc1cb3dee2a4984dc281a9b4bfda686
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-03-29 18:46:06 +0300

    ALL IN REF

--- circle.js
+++ circle.js
@@ -1,75 +1,62 @@
-ymaps.ready(init);
+$(document).ready(function () {
+  ymaps.ready(init);
+})
+
 
 
 function init() {
-        
-      
-    
-	// we have to get response from ajax request
-	var response = {'points':  [
-		{ 'x' : 53.9, 'y' : 27.56667, 'radius': 500},
-		{ 'x' : 53.9, 'y': 27.582709, 'radius': 500 },
-		{ 'x' : 53.9, 'y': 27.598709, 'radius': 500 }
-	]};
-	
-   
-	
-	 
-	
+
+  $.ajax({
+    url: "http://127.0.0.1:4508/api/markers/all",
+    type: "GET",
+    success: function(data) {
+
+    var resp = data.res;
+
     // Создаем карту.
     var myMap = new ymaps.Map("map", {
-        center: [53.9, 27.56667],
-        zoom: 13
-        //controls: []
+        center: [53.890804, 27.537267],
+        zoom: 18,
+        controls: []
     });
 
-    
+    myMap.controls.add(new ymaps.control.ZoomControl())
+    myMap.controls.add(new ymaps.control.SearchControl())
 
+  for(i = 0; i < resp.length; i++) {
 
-	
-	for(i = 0; i < response.points.length; i++) {
-		
-		// Создаем круг.
-		var circle = new ymaps.Circle([
-			// Координаты центра круга.
-			[response.points[i].x, response.points[i].y],
-			// Радиус круга в метрах.
-			response.points[i].radius
-		], {
-			// Описываем свойства круга.
-			// Содержимое балуна.
-			balloonContent: "Радиус круга - 10 км",
-			// Содержимое хинта.
-			hintContent: "Зарази меня"
-		}, {
-			// Задаем опции круга.
-			// Включаем возможность перетаскивания круга.
-			draggable: false,
-			// Цвет заливки.
-			// Последний байт (77) определяет прозрачность.
-			// Прозрачность заливки также можно задать используя опцию "fillOpacity".
-			fillColor: "#DB709377",
-			// Цвет обводки.
-			strokeColor: "#FF0000",
-			// Прозрачность обводки.
-			strokeOpacity: 0.8,
-			// Ширина обводки в пикселях.
-			strokeWidth: 1
-		});
-                
-                
-		
-           
+    // Создаем круг.
+    var circle = new ymaps.Circle([
+      // Координаты центра круга.
+      [resp[i].lt, resp[i].ln],
+      // Радиус круга в метрах.
+      200
+    ], {
+      // Описываем свойства круга.
+      // Содержимое балуна.
+      balloonContent: "200 метров бацил",
+      // Содержимое хинта.
+      hintContent: "Тут можно заболеть!"
+    }, {
+      // Задаем опции круга.
+      // Включаем возможность перетаскивания круга.
+      draggable: false,
+      // Цвет заливки.
+      // Последний байт (77) определяет прозрачность.
+      // Прозрачность заливки также можно задать используя опцию "fillOpacity".
+      fillColor: "#DB709377",
+      // Прозрачность обводки.
+      strokeOpacity: 1,
+      // Ширина обводки в пикселях.
+      strokeWidth: 0
+    });
+     // Добавляем круг на карту.
+    myMap.geoObjects.add(circle);
 
-
-       
-              
-                
-		 // Добавляем круг на карту.
-		myMap.geoObjects.add(circle);
-               
+  }
+  }
+    })
 
 
 	} // end for
-        
-}
+
